bf4d9277c7c952ec0b33372dbc006649062ed3bb
Damon <damonnguyen92@gmail.com>
2019-04-29 14:41:00 -0700

    Refactor and clean up code.

--- client/components/HomeButton.jsx
+++ client/components/HomeButton.jsx
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class HomeButton extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
 
-  handleClick(cb) {
-    cb();
+  static propTypes = {
+    currentPath: PropTypes.string.isRequired,
+    goHome: PropTypes.func.isRequired,
+    goBack: PropTypes.func.isRequired
   }
 
   render() {
+    const { 
+      currentPath,
+      goHome,
+      goBack
+    } = this.props;
+
     return (
       <div className="md-back-btn-icon-container">
-        { this.props.currentPath.slice(0,7) === '/movie/' ? <i className="material-icons md-back-btn-icon" onClick={() => this.handleClick(this.props.goBack)}>keyboard_backspace</i> : null }
-        { this.props.currentPath === '/' || this.props.currentPath.slice(0, 7) === '/movies' ? <i className="material-icons md-home-btn-icon" onClick={() => this.handleClick(this.props.goHome)}>home</i> : null }
+        {
+          (() => {
+            if (currentPath.slice(0, 7) === '/movie/') {
+              return <i className="material-icons md-back-btn-icon" onClick={goBack}>keyboard_backspace</i>
+            } else if (currentPath === '/' || currentPath.slice(0, 7) === '/movies') {
+              return <i className="material-icons md-home-btn-icon" onClick={goHome}>home</i>
+            }
+          })()
+        }
       </div>
     );
   }
