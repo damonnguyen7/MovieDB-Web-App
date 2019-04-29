0f8d165eb617d637790f20eb7c2f84f187a7df3e
Damon <damonnguyen92@gmail.com>
2019-04-29 14:50:19 -0700

    Refactor LoadingBar to be a functional component.

--- client/components/LoadingBar.jsx
+++ client/components/LoadingBar.jsx
@@ -5,31 +5,24 @@ import { css } from '@emotion/core';
 import BarLoader from 'react-spinners/BarLoader';
 
 const override = css`
-    display: block;
-    margin: 0 auto;
-    --box-shadow: 0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12);
+  display: block;
+  margin: 0 auto;
+  --box-shadow: 0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12);
 `;
 
-/**
- * @constructs LoadingBar
- * @props isLoading {Boolean}
- */
-
-class LoadingBar extends Component {
-  render() {
-    return (
-      <BarLoader
-        css={override}
-        width={'100%'}
-        color={'#95afc0'}
-        loading={this.props.isLoading}
-      />
-    );
-  }
+LoadingBar.propTypes = {
+  isLoading: PropTypes.bool.isRequired
 };
 
-LoadingBar.propTypes = {
-  isLoading: PropTypes.bool
+function LoadingBar({ isLoading }) {
+  return (
+    <BarLoader
+      css={override}
+      width={'100%'}
+      color={'#95afc0'}
+      loading={isLoading}
+    />
+  );
 };
 
 export default LoadingBar;
